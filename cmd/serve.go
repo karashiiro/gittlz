686845be5335845bfe0023cf74a6a3097f6ff6c2
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-12-24 23:06:52 -0800

    Allow port overrides in the CLI; export all in Git protocol

--- cmd/serve.go
+++ cmd/serve.go
@@ -14,6 +14,7 @@ var Password string
 var BasePath string
 var Path string
 var Protocol string
+var GitPort int
 
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
@@ -24,30 +25,33 @@ server protocol, which has no authentication scheme.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		switch Protocol {
 		case "git":
-			daemon, err := protocol.StartGit(BasePath, Path)
+			port := getGitPort(9418)
+			daemon, err := protocol.StartGit(port, BasePath, Path)
 			if err != nil {
 				log.Fatalf("Failed to start Git server: %v", err)
 			}
 
-			log.Println("Git server started on port 9418")
+			log.Println("Git server started on port", port)
 
 			err = daemon.Wait()
 			if err != nil {
 				log.Fatalf("Git server failed unexpectedly: %v", err)
 			}
 		case "http":
-			log.Println("Git server started on port 80")
-			err := protocol.StartSmartHTTP(80, Path, Username, Password)
+			port := getGitPort(80)
+			log.Println("Git server started on port", port)
+			err := protocol.StartSmartHTTP(port, Path, Username, Password)
 			if err != nil {
 				log.Fatalf("Git server failed unexpectedly: %v", err)
 			}
 		case "ssh":
-			wh, err := protocol.StartSSH(22, Path, Password)
+			port := getGitPort(22)
+			wh, err := protocol.StartSSH(port, Path, Password)
 			if err != nil {
 				log.Fatalf("Failed to start Git server: %v", err)
 			}
 
-			log.Println("Git server started on port 22")
+			log.Println("Git server started on port", port)
 
 			err = wh.Wait()
 			if err != nil {
@@ -64,8 +68,17 @@ func init() {
 	rootCmd.AddCommand(serveCmd)
 
 	serveCmd.Flags().StringVarP(&Protocol, "protocol", "P", "git", "Git server protocol. Valid options: \"git\", \"http\", \"ssh\".")
+	serveCmd.Flags().IntVar(&GitPort, "git-port", -1, "The port to run the Git server on. Set this to -1 to use the default ports for each protocol.")
 	serveCmd.Flags().StringVarP(&Username, "username", "u", "", "Git username for authentication. Leave this empty to disable the username.")
 	serveCmd.Flags().StringVarP(&Password, "password", "p", "", "Git password for authentication. Leave this empty to disable the password.")
 	serveCmd.Flags().StringVar(&BasePath, "base-path", "/srv/git", "Base path for the Git repositories directory. This acts a prefix removed from --path.")
 	serveCmd.Flags().StringVar(&Path, "path", "/srv/git", "Full path for the Git repositories directory.")
+}
+
+func getGitPort(fallback int) int {
+	if GitPort == -1 {
+		return fallback
+	}
+
+	return GitPort
 }
--- pkg/protocol/git.go
+++ pkg/protocol/git.go
@@ -6,8 +6,8 @@ import (
 	"os/exec"
 )
 
-func StartGit(basePath, path string) (*exec.Cmd, error) {
-	cmd := exec.Command("git", "daemon", "--verbose", "--reuseaddr", fmt.Sprintf("--base-path=%s", basePath), path)
+func StartGit(port int, basePath, path string) (*exec.Cmd, error) {
+	cmd := exec.Command("git", "daemon", "--verbose", "--reuseaddr", "--export-all", fmt.Sprintf("--port=%d", port), fmt.Sprintf("--base-path=%s", basePath), path)
 	// TODO: Pass these through a custom logger that prefixes output appropriately
 	// TODO: Automatically create git-daemon-export-ok?
 	cmd.Stdout = os.Stdout
