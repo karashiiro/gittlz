7b92c4540838d3a54dae2a09acc12734d96f9361
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-12-24 23:13:59 -0800

    Fix receiving being disabled over Git protocol

--- pkg/protocol/git.go
+++ pkg/protocol/git.go
@@ -7,7 +7,7 @@ import (
 )
 
 func StartGit(port int, basePath, path string) (*exec.Cmd, error) {
-	cmd := exec.Command("git", "daemon", "--verbose", "--reuseaddr", "--export-all", fmt.Sprintf("--port=%d", port), fmt.Sprintf("--base-path=%s", basePath), path)
+	cmd := exec.Command("git", "daemon", "--verbose", "--reuseaddr", "--export-all", "--enable=upload-pack", "--enable=upload-archive", "--enable=receive-pack", fmt.Sprintf("--port=%d", port), fmt.Sprintf("--base-path=%s", basePath), path)
 	// TODO: Pass these through a custom logger that prefixes output appropriately
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
